c670481339d2f51fcabe180b89279c0d2609de7b
mineiro1 <34043731+mineiro1@users.noreply.github.com>
2020-04-26 22:28:03 -0300

    Update haldoc.py

--- haldoc.py
+++ haldoc.py
@@ -7,8 +7,8 @@ class docter:
 		self.ses=requests.Session()
 
 	def alodoc(self,num):
-		self.ses.headers.update({'referer':'https://www.alodokter.com/login-alodokter'})
-		req1=self.ses.get('https://www.alodokter.com/login-alodokter')
+		self.ses.headers.update({'referer':'https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy02YzhmZTk3Yi1iODhkLTQ2MWQtYmE3ZC0zODM0MWU2Y2QwYjAiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..OInK0DnJfhbarb-6E_myRQ.PKzlw2vS_4xVWq1yGMP1yqt1dxohwby9Q2wWd79-a0ztpNlpef6oXEgaSS-8fCQDAb9d__AJ_9jEJEoH38-_PptnJrT7J-qIlcANG_8-STpk9qB6dBzVWYPkma4P_Nr53EB5jjaNXOOSKslFqlqr3BbNUgj-ODU7LU2-TjAsbUlMnOhDyOr05oAU3rPZ0NyPqZICCn9JgR7UkP_-oME_MWB3LQrSdYrSfNCCPYkWk1Y0uC8C0hGtIPycmRBFUpmOiHs0uWOEhew9TcmGXr7pKpTVOwCqBXwDdshHuTcukrEj5jA6_FOy5y_Z3GQwuI8REwHfSa6g4p_uiYh3AfVvHVJe26JwrmZhbRtacLvJCH5H86j_oEAYgbQTYxPXvmnNRo-uW6EBOXPTUA7cd76ay_wRqUbGY1unnok0yGeV9VBdHZE5xJ7mfIh5zzTNnpu9urYrgNAciUQxC0g1rocpHzgX7Y5wckigT5nSU8-fW75ARoZKPwnHq_meWhrtg9Mr.vNHdu8G9eQXTbKoc3rsZJCV5oZnDYINrt45tf1PgZZk'})
+		req1=self.ses.get('https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy02YzhmZTk3Yi1iODhkLTQ2MWQtYmE3ZC0zODM0MWU2Y2QwYjAiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..OInK0DnJfhbarb-6E_myRQ.PKzlw2vS_4xVWq1yGMP1yqt1dxohwby9Q2wWd79-a0ztpNlpef6oXEgaSS-8fCQDAb9d__AJ_9jEJEoH38-_PptnJrT7J-qIlcANG_8-STpk9qB6dBzVWYPkma4P_Nr53EB5jjaNXOOSKslFqlqr3BbNUgj-ODU7LU2-TjAsbUlMnOhDyOr05oAU3rPZ0NyPqZICCn9JgR7UkP_-oME_MWB3LQrSdYrSfNCCPYkWk1Y0uC8C0hGtIPycmRBFUpmOiHs0uWOEhew9TcmGXr7pKpTVOwCqBXwDdshHuTcukrEj5jA6_FOy5y_Z3GQwuI8REwHfSa6g4p_uiYh3AfVvHVJe26JwrmZhbRtacLvJCH5H86j_oEAYgbQTYxPXvmnNRo-uW6EBOXPTUA7cd76ay_wRqUbGY1unnok0yGeV9VBdHZE5xJ7mfIh5zzTNnpu9urYrgNAciUQxC0g1rocpHzgX7Y5wckigT5nSU8-fW75ARoZKPwnHq_meWhrtg9Mr.vNHdu8G9eQXTbKoc3rsZJCV5oZnDYINrt45tf1PgZZk')
 		bs1=BS(req1.text,'html.parser')
 		token=bs1.find('meta',{'name':'csrf-token'})['content']
 #		print(token)
@@ -16,12 +16,12 @@ class docter:
 		head={
 			'user-agent':'Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
 			'content-type':'application/json',
-			'referer':'https://www.alodokter.com/login-alodokter',
+			'referer':'https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy02YzhmZTk3Yi1iODhkLTQ2MWQtYmE3ZC0zODM0MWU2Y2QwYjAiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..OInK0DnJfhbarb-6E_myRQ.PKzlw2vS_4xVWq1yGMP1yqt1dxohwby9Q2wWd79-a0ztpNlpef6oXEgaSS-8fCQDAb9d__AJ_9jEJEoH38-_PptnJrT7J-qIlcANG_8-STpk9qB6dBzVWYPkma4P_Nr53EB5jjaNXOOSKslFqlqr3BbNUgj-ODU7LU2-TjAsbUlMnOhDyOr05oAU3rPZ0NyPqZICCn9JgR7UkP_-oME_MWB3LQrSdYrSfNCCPYkWk1Y0uC8C0hGtIPycmRBFUpmOiHs0uWOEhew9TcmGXr7pKpTVOwCqBXwDdshHuTcukrEj5jA6_FOy5y_Z3GQwuI8REwHfSa6g4p_uiYh3AfVvHVJe26JwrmZhbRtacLvJCH5H86j_oEAYgbQTYxPXvmnNRo-uW6EBOXPTUA7cd76ay_wRqUbGY1unnok0yGeV9VBdHZE5xJ7mfIh5zzTNnpu9urYrgNAciUQxC0g1rocpHzgX7Y5wckigT5nSU8-fW75ARoZKPwnHq_meWhrtg9Mr.vNHdu8G9eQXTbKoc3rsZJCV5oZnDYINrt45tf1PgZZk',
 			'accept':'application/json',
-			'origin':'https://www.alodokter.com',
+			'origin':'https://login.vivo.com.br/',
 			'x-csrf-token':token
 		}
-		req2=self.ses.post('https://www.alodokter.com/login-with-phone-number',headers=head,json={"user":{"phone":num}})
+		req2=self.ses.post('https://login.vivo.com.br/loginmarca/appmanager/marca/publico?acesso=paravoce',headers=head,json={"user":{"phone":num}})
 #		print(req2.json())
 		if req2.json()['status'] == 'success':
 			print("[•] Berhasil")

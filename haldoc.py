c15fc833e4e30302ca164b090dbc9b7d2c113dfd
mineiro1 <34043731+mineiro1@users.noreply.github.com>
2020-04-26 22:48:46 -0300

    Update haldoc.py

--- haldoc.py
+++ haldoc.py
@@ -7,8 +7,8 @@ class docter:
 		self.ses=requests.Session()
 
 	def alodoc(self,num):
-		self.ses.headers.update({'referer':'https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy02YzhmZTk3Yi1iODhkLTQ2MWQtYmE3ZC0zODM0MWU2Y2QwYjAiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..OInK0DnJfhbarb-6E_myRQ.PKzlw2vS_4xVWq1yGMP1yqt1dxohwby9Q2wWd79-a0ztpNlpef6oXEgaSS-8fCQDAb9d__AJ_9jEJEoH38-_PptnJrT7J-qIlcANG_8-STpk9qB6dBzVWYPkma4P_Nr53EB5jjaNXOOSKslFqlqr3BbNUgj-ODU7LU2-TjAsbUlMnOhDyOr05oAU3rPZ0NyPqZICCn9JgR7UkP_-oME_MWB3LQrSdYrSfNCCPYkWk1Y0uC8C0hGtIPycmRBFUpmOiHs0uWOEhew9TcmGXr7pKpTVOwCqBXwDdshHuTcukrEj5jA6_FOy5y_Z3GQwuI8REwHfSa6g4p_uiYh3AfVvHVJe26JwrmZhbRtacLvJCH5H86j_oEAYgbQTYxPXvmnNRo-uW6EBOXPTUA7cd76ay_wRqUbGY1unnok0yGeV9VBdHZE5xJ7mfIh5zzTNnpu9urYrgNAciUQxC0g1rocpHzgX7Y5wckigT5nSU8-fW75ARoZKPwnHq_meWhrtg9Mr.vNHdu8G9eQXTbKoc3rsZJCV5oZnDYINrt45tf1PgZZk'})
-		req1=self.ses.get('https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy02YzhmZTk3Yi1iODhkLTQ2MWQtYmE3ZC0zODM0MWU2Y2QwYjAiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..OInK0DnJfhbarb-6E_myRQ.PKzlw2vS_4xVWq1yGMP1yqt1dxohwby9Q2wWd79-a0ztpNlpef6oXEgaSS-8fCQDAb9d__AJ_9jEJEoH38-_PptnJrT7J-qIlcANG_8-STpk9qB6dBzVWYPkma4P_Nr53EB5jjaNXOOSKslFqlqr3BbNUgj-ODU7LU2-TjAsbUlMnOhDyOr05oAU3rPZ0NyPqZICCn9JgR7UkP_-oME_MWB3LQrSdYrSfNCCPYkWk1Y0uC8C0hGtIPycmRBFUpmOiHs0uWOEhew9TcmGXr7pKpTVOwCqBXwDdshHuTcukrEj5jA6_FOy5y_Z3GQwuI8REwHfSa6g4p_uiYh3AfVvHVJe26JwrmZhbRtacLvJCH5H86j_oEAYgbQTYxPXvmnNRo-uW6EBOXPTUA7cd76ay_wRqUbGY1unnok0yGeV9VBdHZE5xJ7mfIh5zzTNnpu9urYrgNAciUQxC0g1rocpHzgX7Y5wckigT5nSU8-fW75ARoZKPwnHq_meWhrtg9Mr.vNHdu8G9eQXTbKoc3rsZJCV5oZnDYINrt45tf1PgZZk')
+		self.ses.headers.update({'referer':'https://login.vivo.com.br/loginmarca/appmanager/marca/publico?acesso=paravoce'})
+		req1=self.ses.get('https://login.vivo.com.br/loginmarca/appmanager/marca/publico?acesso=paravoce')
 		bs1=BS(req1.text,'html.parser')
 		token=bs1.find('meta',{'name':'csrf-token'})['content']
 #		print(token)
@@ -16,12 +16,12 @@ class docter:
 		head={
 			'user-agent':'Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
 			'content-type':'application/json',
-			'referer':'https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy02YzhmZTk3Yi1iODhkLTQ2MWQtYmE3ZC0zODM0MWU2Y2QwYjAiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..OInK0DnJfhbarb-6E_myRQ.PKzlw2vS_4xVWq1yGMP1yqt1dxohwby9Q2wWd79-a0ztpNlpef6oXEgaSS-8fCQDAb9d__AJ_9jEJEoH38-_PptnJrT7J-qIlcANG_8-STpk9qB6dBzVWYPkma4P_Nr53EB5jjaNXOOSKslFqlqr3BbNUgj-ODU7LU2-TjAsbUlMnOhDyOr05oAU3rPZ0NyPqZICCn9JgR7UkP_-oME_MWB3LQrSdYrSfNCCPYkWk1Y0uC8C0hGtIPycmRBFUpmOiHs0uWOEhew9TcmGXr7pKpTVOwCqBXwDdshHuTcukrEj5jA6_FOy5y_Z3GQwuI8REwHfSa6g4p_uiYh3AfVvHVJe26JwrmZhbRtacLvJCH5H86j_oEAYgbQTYxPXvmnNRo-uW6EBOXPTUA7cd76ay_wRqUbGY1unnok0yGeV9VBdHZE5xJ7mfIh5zzTNnpu9urYrgNAciUQxC0g1rocpHzgX7Y5wckigT5nSU8-fW75ARoZKPwnHq_meWhrtg9Mr.vNHdu8G9eQXTbKoc3rsZJCV5oZnDYINrt45tf1PgZZk',
+			'referer':'https://login.vivo.com.br/loginmarca/appmanager/marca/publico?acesso=paravoce',
 			'accept':'application/json',
 			'origin':'https://login.vivo.com.br/',
 			'x-csrf-token':token
 		}
-		req2=self.ses.post('https://login.vivo.com.br/loginmarca/appmanager/marca/publico?acesso=paravoce',headers=head,json={"user":{"phone":num}})
+		req2=self.ses.post('https://br.mobileconnect.telefonica.com/br/authrouter/authenticate?jwt=eyJraWQiOiJ0ZGFmLWFwaS1hdXRoc2VydmVyIiwiY29yciI6IkFVUy1kOTFhMzFjNC03MGJjLTQzNzUtOWM3NC0wYmQ0OWJiNDFhZWIiLCJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..CaYvKvXa9-P_nyWojU80oA.5U8Yc_nWLUnVpL3TK4RHlcQ-NYDq_vpiswzNfOUzIJcKlYQ_oAUzk-vb0a_RtbXqdS01xI2nJn4xO4lB42PTRRG_tatQ0_znoy2BY8EWA8oC_SHvSw8et_P_NaOjOckhFP5iMlGQEZ9jaSsfQhWPnPLq1cPy6XAcWwtYj9-bggzyx-JE5FghUtaPXsbgG7UsTdgHYj3XLa2w60VHZLfXxJBNyUPMxHgxnsI4I2oT21KlUTSbV5IxOjP_fsTMb1BCpkow9bd0uzZ-2r_oM-YO76x1zqDF6dzRIQ0Ly8Iq8auVHapyu8dN22BHYxUkmVo22DzfJufdxO_LbkkpD7XF0kPNjRPiKLm2_fCo0c0f-Xh5khXQi9n2BK7oqZdrOqqViOpYHdDZ0SgCM2ENL52DGoKymk4e2EOlbfMNgtpqVhuGC6JoSOMve4GcewXMQAdHJ8I7I_WatgfvHjIt-1EjLcmziK22n2rUuybzI-DCnhBe3mUnkBtKnvITUTdJDfKa.7qqKhCeiabBbuHV4ugFbCIhI6zLecdR-YjvwSv96zRA',headers=head,json={"user":{"phone":num}})
 #		print(req2.json())
 		if req2.json()['status'] == 'success':
 			print("[•] Berhasil")
